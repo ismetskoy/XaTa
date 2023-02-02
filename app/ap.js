87ca235347a5004e07d2356227014e4086b469d9
Ivan <felhat@bk.ru>
2023-02-02 21:24:15 +0300

    up

--- app/ap.js
+++ app/ap.js
@@ -1,16 +1,8 @@
-$(document).ready(function() {
-
-	$("#form").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$(this).find("input").val("");
-			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
-			$("#form").trigger("reset");
-		});
-		return false;
-	});
-	
+$('.openmodal').click(function (e) {
+  e.preventDefault();
+  $('.modal').addClass('opened');
+});
+$('.closemodal').click(function (e) {
+  e.preventDefault();
+  $('.modal').removeClass('opened');
 });
